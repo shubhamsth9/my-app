f8dae9653f92e764aa38d295a1a6e784b0408576
shubhamxthakur <stfushubham@gmail.com>
2022-05-02 23:51:53 +0530

    comiit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, {useState, useEffect}  from 'react';
+import ContactCard from './contactCard';
+import './App.css'
 
 const App = () => {
+  
+  const [results, setResults] = useState([]);
 
-  const title = "Namaste";
+  useEffect(()=>{
+    fetch("https://randomuser.me/api/?results=3")
+    .then(response => response.json())
+    .then(data => {
+      setResults(data.results)
+    });
+  }, [])
 
   return(
-    <div>
-      <h1>{title}</h1>
-      <h2 onClick={() => alert("Kyiu chua usko?")}>
-        Ye mera React app hai! 
-      </h2>
+    <div className='container'>
+      {results.map((result, index)=> {
+        return(
+          <ContactCard key = {index}
+            name = {result.name.first +" "+ result.name.last} 
+            email = {result.email}
+            age = {result.dob.age}
+            img = {result.picture.large} />
+        )
+      })}
     </div>
   )
 }
---
+++ src/contactCard.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+
+const ContactCard = (props) => {
+
+    const [showAge, setShowAge] = useState(false);
+
+    return(
+    <div className="contact-card">
+      <img src={props.img} alt='profile' />
+    <div className='user-details'>
+      <p>Name: {props.name}</p>
+      <p>Email: {props.email}</p>
+      <button onClick={()=>setShowAge(!showAge)}>Show Age</button>
+      {showAge && <p>Age: {props.age}</p> }
+    </div>
+    </div>
+    )
+}
+
+export default ContactCard;
